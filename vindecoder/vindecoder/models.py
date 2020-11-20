2a310ccb9b4894bcb11063ed3a753382d6cfb4b1
Ameya Gore <ameya@localhost.localdomain>
2020-11-20 17:51:14 +0530

    Restored project structure

--- vindecoder/vindecoder/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Vin(models.Model):
-    value = models.CharField(max_length=30)
-    valueId = models.CharField(max_length=30)
-    variable = models.CharField(max_length=30)
-    variableId = models.CharField(max_length=30)
--- vindecoder/vindecoder/serializer.py
+++
@@ -1,8 +0,0 @@
-from vindecoder.models import Vin
-from rest_framework import serializers
-
-
-class VinSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Vin
-        fields = ['variable', 'variableId', 'value', 'valueId']
